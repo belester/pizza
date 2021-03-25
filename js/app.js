f64f6e6cbefbdc1e79280e22bded31d41e61bc97
francisco belester acosta lujano <franciscobelester@gmail.com>
2021-03-25 11:38:20 -0600

    cerrar el menu con elementos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 //MENU RESPONSIVE
 var btnMenuOpen =  document.getElementById("btnMenuOpen"),
     btnMenuClose = document.getElementById("btnMenuClose"),
-    btnResponsive = document.getElementById("menuBar");
+    btnResponsive = document.getElementById("menuBar"),
+    enlaces = document.getElementById("enlaces");
 // CLICK ABRIR
     btnMenuOpen.addEventListener("click", function (){
         btnResponsive.classList.add("active");
@@ -9,5 +10,10 @@ var btnMenuOpen =  document.getElementById("btnMenuOpen"),
 
 // CLICK CERRAR
     btnMenuClose.addEventListener("click", function (){
+        btnResponsive.classList.remove("active");
+    })
+// CERRAR MENU CON ELEMENTOS
+    enlaces.addEventListener("click", function () {
+        btnResponsive.style.transitionDelay = "0.05s";
         btnResponsive.classList.remove("active");
     })
